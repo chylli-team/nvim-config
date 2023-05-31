a014d928932be8428336c4bed431d660b011722b
chylli-deriv <chylli@binary.com>
2023-06-01 00:20:11 +0800

    add tags path

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -39,3 +39,4 @@ opt.splitright = true -- split vertical window to the right
 opt.splitbelow = true -- split horizontal window to the bottom
 
 opt.iskeyword:append("-") -- consider string-string as whole word
+opt.tags = "./tags,tags,~/tags" -- set tags file to be in the root of the project
